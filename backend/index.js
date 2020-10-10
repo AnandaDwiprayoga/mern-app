c73be79075150520c398a1be14553b4759fa3377
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2020-10-11 01:39:43 +0700

    example using route in express

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,35 @@
+const { request } = require('express');
 const express = require('express');
 
 const app = express();
-app.use(() => {
-    console.log("express loaded");
-    console.log("express loaded again");
-    console.log("express loaded triple");
+// untuk router menangkap perpindahan halaman
+const router = express.Router();
+
+
+//router.use menerima semua method request, get,post,dll
+router.use('/bio', (request, response, next) => {
+    console.log("url : " + request.originalUrl);
+    console.log("method : " + request.method);
+
+    //mengembalikan response json
+    const dummyResponse = {
+        nama : "ananda dwi prayoga",
+        klass: "MI-3D"
+    };
+
+    response.json(dummyResponse)
+    //next digunakan untuk mengecek route lagi yang sama dan menjalankannya
+    next()
+});
+
+
+//router.get khusus menanagani method get, contoh lainnya router.delete, router.put, dll
+router.get('/user', (request, response, next) => {
+    response.json('this is from get method')
 })
+
+
+//homepage express di set sesuai router
+app.use('/', router);
 
 app.listen(3300);
